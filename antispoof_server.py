f7b0afd9f18eb8066fdc94e3c5561b43a48a54f8
Nathan4Andrade <nathanandrade20@gmail.com>
2025-05-05 10:47:58 -0300

    fix: cors

--- antispoof_server.py
+++ antispoof_server.py
@@ -4,8 +4,10 @@ from flask import Flask, request, jsonify
 from io import BytesIO
 from PIL import Image
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/predict', methods=['POST'])
 def predict():
